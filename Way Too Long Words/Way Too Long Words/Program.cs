1308f9aba7ae2287d7b2727dd3bce0a9c7927dd1
HongDiep1806 <diep.tuthi.cit21@eiu.edu.vn>
2024-02-21 22:19:41 +0700

    Way Too Long Words

--- Way Too Long Words/Way Too Long Words/Program.cs
+++ Way Too Long Words/Way Too Long Words/Program.cs
@@ -1,10 +1,65 @@
-﻿namespace Way_Too_Long_Words
+﻿using System.Text;
+
+namespace Way_Too_Long_Words
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Reader reader = new Reader();
+
+            var numOfWord = reader.NextInt();
+            StringBuilder result = new StringBuilder();
+            while (numOfWord != 0)
+            {
+                var word = reader.Next();
+                if (word.Length > 10)
+                {
+                    result.Append(AbbWord(word));
+                }
+                else
+                {
+                    result.Append(word);
+                }
+
+                result.Append("\n");
+                numOfWord--;
+            }
+            Console.Write(result.ToString());
+        }
+        static StringBuilder AbbWord(string word)
+        {
+            StringBuilder result = new StringBuilder();
+            //  result.Append("");
+            char firstChar = word[0];
+            char lastChar = word[word.Length - 1];
+            int lengthBetween = word.Length - 2;
+            result.Append($"{firstChar}{lengthBetween}{lastChar}");
+            return result;
+        }
+
+    }
+    class Reader
+    {
+        private int index = 0;
+        private string[] tokens;
+        public string Next()
+        {
+            while (tokens == null || tokens.Length <= index)
+            {
+                tokens = Console.ReadLine().Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                index = 0;
+            }
+            return tokens[index++];
+        }
+        public int NextInt()
+        {
+            return int.Parse(Next());
+        }
+
+        public double NextDouble()
+        {
+            return double.Parse(Next());
         }
     }
 }
