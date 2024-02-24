ba173fcd0b16cf81e6be40d8f2bba981793a5c09
HongDiep1806 <diep.tuthi.cit21@eiu.edu.vn>
2024-02-24 21:21:33 +0700

    fixed Stones on Table

--- Stones on the Table/Stones on the Table/Program.cs
+++ Stones on the Table/Stones on the Table/Program.cs
@@ -5,63 +5,46 @@
         static void Main(string[] args)
         {
             Reader reader = new Reader();
-            var numOfStone = reader.NextInt();
-            var colors = new string[numOfStone];
-            //get input
-            for (int i = 0; i < numOfStone; i++)
+
+            int n = int.Parse(reader.Next());
+            string s = reader.Next();
+
+            int count = 0;
+
+            for (int i = 1; i < n; i++)
             {
-                colors[i] = reader.Next();
-            }
-            //
-            var count = 0;
-            for (int i = 0; i < numOfStone; i++)
-            {
-                for (int j = i; j < numOfStone-1; j++)
+                if (s[i] == s[i - 1])
                 {
-                    bool checkDuplicate = check(colors[i], colors[j]);
-                    if(checkDuplicate == true)
-                    {
-                        count++;
-                    }
+                    count++;
                 }
             }
+
             Console.WriteLine(count);
 
         }
 
-        public static Boolean check(string s1, string s2)
+        class Reader
         {
-            bool check = false;
-            if (s1.Equals(s2))
+            private int index = 0;
+            private string[] tokens;
+            public string Next()
             {
-                check = true;
-                return check;
+                while (tokens == null || tokens.Length <= index)
+                {
+                    tokens = Console.ReadLine().Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                    index = 0;
+                }
+                return tokens[index++];
+            }
+            public int NextInt()
+            {
+                return int.Parse(Next());
             }
 
-            return check;
-        }
-    }
-    class Reader
-    {
-        private int index = 0;
-        private string[] tokens;
-        public string Next()
-        {
-            while (tokens == null || tokens.Length <= index)
+            public double NextDouble()
             {
-                tokens = Console.ReadLine().Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
-                index = 0;
+                return double.Parse(Next());
             }
-            return tokens[index++];
-        }
-        public int NextInt()
-        {
-            return int.Parse(Next());
-        }
-
-        public double NextDouble()
-        {
-            return double.Parse(Next());
         }
     }
-}
+    }
