6f918d207459079834dee3781516efcf8dbfda64
HongDiep1806 <diep.tuthi.cit21@eiu.edu.vn>
2024-02-27 18:46:27 +0700

    doing [Anton and Danik]. [Petya and Strings] exercise

---
+++ Anton and Danik/Anton and Danik/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace Anton_and_Danik
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Reader reader = new Reader();
+            var numOfGames = reader.NextInt();
+            var outCome = reader.Next();
+            var countAnton = 0;
+            var countDanik = 0;
+            for (int i = 0; i < numOfGames; i++)
+            {
+                if (outCome[i] == 'A')
+                {
+                    countAnton++;
+                }
+                else
+                {
+                    countDanik++;
+                }
+            }
+            if(countAnton > countDanik)
+            {
+                Console.WriteLine("Anton");
+            }
+            else
+            {
+                if(countAnton < countDanik)
+                {
+                    Console.WriteLine("Danik");
+                }
+                else
+                {
+                    Console.WriteLine("Friendship");
+                }
+            }
+        }
+    }
+    class Reader
+    {
+        private int index = 0;
+        private string[] tokens;
+        public string Next()
+        {
+            while (tokens == null || tokens.Length <= index)
+            {
+                tokens = Console.ReadLine().Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                index = 0;
+            }
+            return tokens[index++];
+        }
+        public int NextInt()
+        {
+            return int.Parse(Next());
+        }
+
+        public double NextDouble()
+        {
+            return double.Parse(Next());
+        }
+    }
+}
